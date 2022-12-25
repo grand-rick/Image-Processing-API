05e57d43482e9a5d11491fd0cabcf2e622cdd6dd
Patrick Kabuga <kabugap02@gmail.com>
2022-12-26 00:43:25 +0300

    Moved workflow to routers and removed dependability of utilities

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import router from './routes/router';
 import dotenv from 'dotenv';
@@ -9,15 +9,9 @@ const app = express();
 const port = process.env.port;
 
 app.use(morgan('common'));
-app.use(express.static('assets'));
+// app.use(express.static('assets'));
 
 app.use('/', router);
-
-const rootDir = process.env.rootDir;
-
-app.get('/image', (req: Request, res: Response) => {
-    res.sendFile('assets/images/fjord.jpg', { root: rootDir });
-});
 
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);
--- src/routes/convert/convert.ts
+++ src/routes/convert/convert.ts
@@ -1,18 +1,28 @@
-import express from 'express';
-import convertImage from '../utilities/resize';
+import express, {Request, Response, NextFunction} from 'express';
+import sharp from 'sharp';
 
 const convert = express.Router();
-const inputFile = './images/fjord.jpg';
-const outputFile = 'resized.jpg';
 
-convert.get('/', (req, res) => {
-    res.send('Converting in progress');
-    convertImage(inputFile, outputFile);
-    // const fileName = req.query.fileName;
-    // const width = req.query.width;
-    // const height = req.query.height;
-    
-    // console.log(`Filename: ${fileName}, width: ${width}, ${height}`);
+const rootDir = process.env.rootDir;
+
+const progress = (req: Request, res: Response, next: NextFunction) => {
+    res.send('Conversion in progress');
+    next();
+}
+
+convert.get('/', progress, async (req: Request, res: Response) => {
+    const inputFileName = req.query.fileName;
+
+    const inputFile = `${rootDir}/assets/images/${inputFileName}.jpg`
+    const width = req.query.width;
+    const height = req.query.height;
+    const outputFile = `${rootDir}/assets/images/${inputFile}_${width}_${height}.jpg`;
+    try {
+        await sharp(inputFile).resize(300, 400).toFile(outputFile);
+    } catch(err) {
+        console.log(err);
+    }
+    res.sendFile(`assets/images/${outputFile}`, { root: rootDir });
 });
 
 export default convert;
