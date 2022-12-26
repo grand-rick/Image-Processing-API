218bcbe12f7667ec5cc60897c682e3a5d7f68b9d
Patrick Kabuga <kabugap02@gmail.com>
2022-12-26 21:55:39 +0300

    Checking if image exists or not

--- src/routes/convert/convert.ts
+++ src/routes/convert/convert.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import resizeImage from '../utilities/resize';
+import fs from 'fs';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,18 +10,20 @@ const convert = express.Router();
 const rootDir = process.env.rootDir;
 
 convert.get('/', async (req: Request, res: Response) => {
-	const inputFileName = req.query.fileName as unknown as string;
-	const width = req.query.width as unknown as string;
-	const height = req.query.height as unknown as string;
-
-	const outputFileName = `${inputFileName}_${width}_${height}`;
+	const inputFileName = (req.query.fileName as unknown) as string;
+	const width = (req.query.width as unknown) as string;
+	const height = (req.query.height as unknown) as string;
 
 	const inputFile = `${rootDir}/assets/images/${inputFileName}.jpg`;
+	const outputFileName = `${inputFileName}_${width}_${height}`;
 	const outputFile = `${rootDir}/assets/thumbs/${outputFileName}.jpg`;
 
-	resizeImage(inputFile, width, height, outputFile);
-
-	res.sendFile(`${outputFile}`);
+	if (fs.existsSync(inputFile)) {
+		resizeImage(inputFile, width, height, outputFile);
+		res.sendFile(`${outputFile}`);
+	} else {
+		res.send('Image does not exist');
+	}
 });
 
 export default convert;
